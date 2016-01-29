07d485ecad3d67f580dba5d565be156ef439d19f
Juan Carlos Farías Arredondo <gaxel97@gmail.com>
2016-01-29 16:21:49 -0600

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Juan Carlos Farías Arredondo on 29/01/16.
+//  Copyright © 2016 Comisión Federal de Electricidad DCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
